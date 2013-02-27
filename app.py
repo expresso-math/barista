06f622bb9ade8a4c7847624e97d830d8e22335c8
Josef Lange <josef.d.lange@me.com>
2013-02-27 15:37:52 -0800

    Added basic test app.

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask
+from flask.ext import restful
+
+app = Flask(__name__)
+api = restful.Api(app)
+
+class HelloWorld(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+class Presidents(restful.Resource):
+    def get(self):
+        return { '0': {'name': 'Washington, George' },
+            '1': {'name': 'Adams, John' },
+                '2': {'name': 'Jefferson, Thomas'} }
+
+api.add_resource(Presidents, '/presidents')
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
