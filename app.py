fedd3e41bde91eecd848495e11370e599b7ec960
Josef Lange <josef.d.lange@me.com>
2013-02-28 23:00:02 -0800

    Actually fixed Heroku nonlaunch. Had to grab the right port from their ENV (defaulting to the flask-norm of 5000).

--- app.py
+++ app.py
@@ -20,4 +20,5 @@ class Presidents(restful.Resource):
 api.add_resource(Presidents, '/presidents')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
