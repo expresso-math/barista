8400c5f4fbae4c851aabb96370ad28158c6e5c0b
Josef Lange <josef.d.lange@me.com>
2013-03-03 20:59:00 -0800

    Fixed tabs and implemented API shell for our determined RESTful API.

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import datetime
 from flask import Flask
 from flask.ext import restful
-import boto
 
 import barista_utilities as util
 
@@ -10,46 +9,81 @@ app = Flask(__name__)
 api = restful.Api(app)
 
 sessions = []
-images = []
 sessions_images = {}
 
-s3_conn = boto.connect_s3('AKIAJQTURA4O3CYWYPPA', 'fe3AKimi8nLhGiT5VRJbZvQ1KlqImzqkxUHzW02P')
-drawing_bucket = s3_conn.get_bucket('expresso-drawings')
-print str(drawing_bucket.list())
+AWS_PUBLIC_KEY = 'AKIAJQTURA4O3CYWYPPA'
+AWS_SECRET_KEY = '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'
 
-class SessionManagement(restful.Resource):
+class Session(restful.Resource):
     def get(self, session_id=''):
-		if session_id not in sessions:
-			name = util.generate_session_name()
-			while name in sessions:
-				name = util.generate_session_name()
-			sessions.append(name)
-			sessions_images[name] = []
-		else:
-			name = session_id
-		print "Session Name: " + str(name)
-		print "All Sessions: " + str(sessions)
-		return name
+        
+        # Setup for the return.
+        return_data = { 'session_name' : '', 'images': [] }
+        name = ''
+
+        if session_id:
+            # A session ID has been defined.
+            if session_id not in sessions:
+                # The defined session ID does not exist as a current session,
+                # so we make one given the session name.
+                name = session_id
+                self.add_session(name)
+            else:
+                # The defined session ID does exist already, so we just forward
+                # this to the next step.
+                name = session_id
+        else:
+            # No session ID was defined, so we make one up for the client.
+            name = util.generate_session_name()
+            while name in sessions:
+                name = util.generate_session_name()
+            self.add_session(name)
+
+        return_data['session_name'] = name
+        return_data['images'] = sessions_images[name]
+
+        print "Session_id passed in is '" + session_id + "'."
+        print "Sessions stored are " + str(sessions) + "."
+
+        return return_data
+
+    def add_session(self, session_id):
+        sessions.append(session_id)
+        sessions_images[session_id] = []
+
+class Expression(restful.Resource):
+    def get(self, session_id):
+        return 'this is new expression belonging to ' + session_id + '!!'
 
 class Image(restful.Resource):
-	def get(self, session_id):
-		session_images = sessions_images[session_id]
-		most_recent = session_images[len(session_images)-1]
-		message = 'The most recent image, ' + str(most_recent) + ', belonging to ' + session_id
-		return message
-	def put(self, session_id):
-		images.append('an image from ' + str(datetime.datetime.now()))
-		print str(sessions_images)
-		session_images = sessions_images[session_id]
-		image_id = len(images)-1
-		session_images.append(image_id)
-		sessions_images[session_id] = session_images
-		message = 'Added image ' + str(image_id) + ' to session ' + session_id 
-		return message
+    def get(self, session_id):
+        return 'you got an image!'
+    def post(self, session_id):
+        return 'you posted an image!'
+
+class SymbolSet(restful.Resource):
+    def get(self, expression_id):
+        return 'you got a symbol set!'
+    def post(self, expression_id):
+        return 'you set a symbol set!'
+
+class EquationSet(restful.Resource):
+    def get(self, expression_id):
+        return 'you got a equation set!'
+    def post(self, expression_id):
+        return 'you set a equation set!'
+
+class Equation(restful.Resource):
+    def get(self, expression_id):
+        return 'this is an equation!'
 
 # Set up resources in API.
-api.add_resource(SessionManagement, '/','/<string:session_id>')
-api.add_resource(Image, '/<string:session_id>/image')
+api.add_resource(Session, '/','/session', '/<string:session_id>', '/session/<string:session_id>')
+api.add_resource(Expression, '/<string:session_id>/expression')
+api.add_resource(Image, '/expression/<string:expression_id>/image')
+api.add_resource(SymbolSet, '/expression/<string:expression_id>/symbolset')
+api.add_resource(EquationSet, '/expression/<string:expression_id>/equationset')
+api.add_resource(Equation, '/expression/<string:expression_id>/equation')
 
 # Run the app, getting the proper port from an ENV
 # if set, otherwise defaulting to 5000.
--- barista_utilities.py
+++ barista_utilities.py
@@ -1,12 +1,12 @@
 import random
 
-adjectives 	= ['iced', 'tall', 'short', 'grande', 'nonfat', 'soy', 'vanilla', 'extra-hot', 'breve', 'cinnamon', 'dry']
-nouns		= ['mocha', 'latte', 'cappuccino', 'macchiato', 'americano', 'steamer', 'frappucino']
+adjectives  = ['iced', 'tall', 'short', 'grande', 'nonfat', 'skinny', 'almond', 'soy', 'vanilla', 'extra-hot', 'breve', 'cinnamon', 'dry']
+nouns       = ['mocha', 'latte', 'cappuccino', 'americano', 'steamer', 'macchiato']
 
 def generate_session_name(): 
-	adjective = adjectives[random.randint(0,len(adjectives)-1)]
-	noun      = nouns[random.randint(0,len(nouns)-1)]
-	number    = str(random.randint(0, 9))+str(random.randint(0, 9))+str(random.randint(0, 9))+str(random.randint(0, 9))
-	name = '' + str(adjective) + '-' + str(noun) + '-' + str(number) + ''
-	print name
-	return name
+    adjective = adjectives[random.randint(0,len(adjectives)-1)]
+    noun      = nouns[random.randint(0,len(nouns)-1)]
+    number    = str(random.randint(0, 9))+str(random.randint(0, 9))+str(random.randint(0, 9))+str(random.randint(0, 9))
+    name = '' + str(adjective) + '-' + str(noun) + '-' + str(number) + ''
+    print name
+    return name
