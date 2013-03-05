a451cb9f54d5431aa14abde338af2c8659305b37
Josef Lange <josef.d.lange@me.com>
2013-03-04 18:04:53 -0800

    Uploading of a file sort of works -- relies on Flask code, not Flask-RESTful, but seems to work.

--- app.py
+++ app.py
@@ -1,33 +1,52 @@
-import os
-import datetime
-from flask import Flask
+import os, datetime, md5
+
+from flask import Flask, request
 from flask.ext import restful
+from flask.ext.restful import fields
+
+from werkzeug import secure_filename
+
+# from PIL import Image
 
 import barista_utilities as util
+
+UPLOAD_FOLDER = './uploaded_images'
+ALLOWED_EXTENSIONS = set(['png'])
 
 app = Flask(__name__)
 api = restful.Api(app)
 
-sessions = []
-sessions_images = {}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Temporary runtime variables for data. Will be removed once we come up
+# with a more permanent data store.
+sessions = {}
+expressions = {}
 
 AWS_PUBLIC_KEY = 'AKIAJQTURA4O3CYWYPPA'
 AWS_SECRET_KEY = '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'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+class Callable:
+    def __init__(self, a_callable):
+        self.__call__ = a_callable
 
 class Session(restful.Resource):
     def get(self, session_id=''):
         
         # Setup for the return.
-        return_data = { 'session_name' : '', 'images': [] }
+        return_data = { 'session_name' : '', 'expressions': [] }
         name = ''
 
         if session_id:
             # A session ID has been defined.
-            if session_id not in sessions:
+            if not Session.session_exists(session_id):
                 # The defined session ID does not exist as a current session,
                 # so we make one given the session name.
                 name = session_id
-                self.add_session(name)
+                Session.add_session(name)
             else:
                 # The defined session ID does exist already, so we just forward
                 # this to the next step.
@@ -35,42 +54,73 @@ class Session(restful.Resource):
         else:
             # No session ID was defined, so we make one up for the client.
             name = util.generate_session_name()
-            while name in sessions:
+            while Session.session_exists(name):
                 name = util.generate_session_name()
-            self.add_session(name)
+            Session.add_session(name)
 
         return_data['session_name'] = name
-        return_data['images'] = sessions_images[name]
-
-        print "Session_id passed in is '" + session_id + "'."
-        print "Sessions stored are " + str(sessions) + "."
+        return_data['expressions'] = sessions[name]
 
         return return_data
 
-    def add_session(self, session_id):
-        sessions.append(session_id)
-        sessions_images[session_id] = []
+    def add_session(session_id):
+        sessions[session_id] = []
+    # Make it a "Class method"
+    add_session = Callable(add_session)
+
+    def session_exists(session_id):
+        return session_id in sessions
+    # Make it a "Class method"
+    session_exists = Callable(session_exists)
 
 class Expression(restful.Resource):
     def get(self, session_id):
-        return 'this is new expression belonging to ' + session_id + '!!'
+        if Session.session_exists(session_id):
+            new_expression = self.add_expression(session_id)
+            return new_expression
+        else:
+            return restful.abort(404)
+
+    def add_expression(session_id):
+        # Make (a relatively) unique identifier for this expression.
+        now = datetime.datetime.now()
+        m = md5.new(str(now))
+        expression_id = m.hexdigest()
+
+        # Add this expression to the session.
+        sessions[session_id].append(expression_id)
+        print str(sessions[session_id])
+
+        # Create the data for the expression
+        expressions[expression_id] = [] # Empty for now, will be full soon enough.
+
+        return expression_id
+    # Make it a "Class method"
+    add_expression = Callable(add_expression)
 
 class Image(restful.Resource):
-    def get(self, session_id):
-        return 'you got an image!'
-    def post(self, session_id):
-        return 'you posted an image!'
+    def get(self, expression_id):
+        return 'This is the image for ' + expression_id + '.'
+    def post(self, expression_id):
+        the_file = request.files['filedata'] # NOTE: Not sure if this will change client to client.
+        if the_file and allowed_file(the_file.filename):
+            filename = secure_filename(the_file.filename)
+            filenameonly, extension = os.path.splitext(filename)
+            savename = expression_id + extension
+            the_file.save(os.path.join(app.config['UPLOAD_FOLDER'], savename))
+        print 'Posting the given image into ' + expression_id + '\'s data.'
+        return expression_id + '.png Saved.', 201
 
 class SymbolSet(restful.Resource):
     def get(self, expression_id):
         return 'you got a symbol set!'
-    def post(self, expression_id):
+    def put(self, expression_id):
         return 'you set a symbol set!'
 
 class EquationSet(restful.Resource):
     def get(self, expression_id):
         return 'you got a equation set!'
-    def post(self, expression_id):
+    def put(self, expression_id):
         return 'you set a equation set!'
 
 class Equation(restful.Resource):
